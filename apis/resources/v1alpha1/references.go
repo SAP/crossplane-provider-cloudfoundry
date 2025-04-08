77d9aa7d09c9613df991be16320908f9c997c98c
Daniel Lou <daniel.lou@sap.com>
2025-04-08 15:28:33 +0200

    update SpaceRef

--- apis/resources/v1alpha1/references.go
+++ apis/resources/v1alpha1/references.go
@@ -2,18 +2,22 @@ package v1alpha1
 
 import v1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 
-// SpaceRef is a struct that represents the reference to a Space CR.
+// SpaceRef defines a reference to a Cloud Foundry space
 type SpaceRef struct {
-	// Space associated guid.
+	// The `guid` of the Cloud Foundry space. This field is typically populated using references specified in `spaceRef`, `spaceSelector`, or `spaceName`.
 	// +crossplane:generate:reference:type=Space
 	// +crossplane:generate:reference:extractor=github.com/SAP/crossplane-provider-cloudfoundry/apis/resources.ExternalID()
 	Space *string `json:"space,omitempty"`
 
-	// Reference to a Space CR to populate space.
+	// The name of the Cloud Foundry space to lookup the `guid` of the Space. Use `spaceName` only when the reference Space is not managed by Crossplane.
+	// +kubebuilder:validation:Optional
+	SpaceName *string `json:"spaceName,omitempty"`
+
+	// Reference to a `Space` CR to lookup the `guid` of the Cloud Foundry space. Preferred if the reference space is managed by Crossplane.
 	// +kubebuilder:validation:Optional
 	SpaceRef *v1.Reference `json:"spaceRef,omitempty"`
 
-	// Selector for a Space CR to populate space.
+	// Selector for a `Space` CR to lookup the `guid` of the Cloud Foundry space. Preferred if the reference space is managed by Crossplane.
 	// +kubebuilder:validation:Optional
 	SpaceSelector *v1.Selector `json:"spaceSelector,omitempty"`
 	// Fields relevant  for managed service instances
