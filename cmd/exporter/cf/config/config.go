d280ae059c5141071872be3427e7f3702d3b0b48
gergely-szabo-sap <gergely.szabo@sap.com>
2025-11-04 17:18:42 +0100

    adding '--use-cf-login' flag

--- cmd/exporter/cf/config/config.go
+++ cmd/exporter/cf/config/config.go
@@ -2,24 +2,31 @@ package config
 
 import (
 	"context"
+	"log/slog"
 
 	"github.com/SAP/crossplane-provider-cloudfoundry/internal/exporttool/cli/configparam"
 
 	"github.com/cloudfoundry/go-cfclient/v3/config"
 )
 
-func Get(ctx context.Context, apiUrlParam, usernameParam, passwordParam *configparam.StringParam) (*config.Config, error) {
-	apiUrl, err := apiUrlParam.ValueOrAsk(ctx)
-	if err != nil {
-		return nil, err
+func Get(ctx context.Context, useCfLoginMethod *configparam.BoolParam, apiUrlParam, usernameParam, passwordParam *configparam.StringParam) (*config.Config, error) {
+	if useCfLoginMethod.Value() {
+		slog.Debug("log in to CF using the CF login method")
+		return config.NewFromCFHome()
+	} else {
+		apiUrl, err := apiUrlParam.ValueOrAsk(ctx)
+		if err != nil {
+			return nil, err
+		}
+		username, err := usernameParam.ValueOrAsk(ctx)
+		if err != nil {
+			return nil, err
+		}
+		password, err := passwordParam.ValueOrAsk(ctx)
+		if err != nil {
+			return nil, err
+		}
+		slog.Debug("log in to CF using credentials", "url", apiUrl, "username", username)
+		return config.New(apiUrl, config.UserPassword(username, password))
 	}
-	username, err := usernameParam.ValueOrAsk(ctx)
-	if err != nil {
-		return nil, err
-	}
-	password, err := passwordParam.ValueOrAsk(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return config.New(apiUrl, config.UserPassword(username, password))
 }
