905f78faa67b6cc71be94b23c0dbc3a5e099a3b3
gergely-szabo-sap <gergely.szabo@sap.com>
2026-03-02 14:44:53 +0100

    adding flake check

--- flake.nix
+++ flake.nix
@@ -7,31 +7,53 @@
   };
 
   outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-      ];
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, lib, system, ... }: {
-        packages = rec {
-          exporter = pkgs.buildGoModule {
-            pname = "xpcf";
-            version = "0.0.1-alpha1";
-            ldflags = ["-X main.ShortName=xpcf"];
-            src = ./.;
-            subPackages = ["cmd/exporter"];
-            vendorHash = "sha256-HiWXSvLwRzt1/wMl2LQfwrBPoRpsl5E3TsN/1N0PGWs=";
-            # vendorHash = lib.fakeHash;
-          };
-          xpcf = pkgs.runCommand "xpcf" {} ''
-              mkdir -p $out/bin
-              cp ${exporter}/bin/exporter $out/bin/xpcf
-            '';
-        };
-        apps.exporter = {
-          type = "app";
-          program = "${self'.packages.xpcf}/bin/xpcf";
+    let
+      exporter-cli = {
+        name = "xpcf";
+        version = "0.0.1-alpha1";
+        meta = lib: {
+          description = "xpcf is a CLI tool for exporting existing resources as Crossplane managed resources";
+          homepage = "https://github.com/SAP/crossplane-provider-cloudfoundry";
+          license = lib.licenses.asl20;
         };
       };
-      flake = {};
-    };
+    in
+      flake-parts.lib.mkFlake { inherit inputs; } {
+        imports = [
+        ];
+        systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+        perSystem = { config, self', inputs', pkgs, lib, system, ... }: {
+          packages = rec {
+            exporter = pkgs.buildGoModule {
+              inherit (exporter-cli) version;
+              pname = exporter-cli.name;
+              ldflags = ["-X main.ShortName=${exporter-cli.name}"];
+              src = with lib; fileset.toSource {
+                root = ./.;
+                fileset = fileset.unions [
+                  (fileset.fromSource (sources.sourceFilesBySuffices ./. [".go"]))
+                  ./go.mod
+                  ./go.sum
+                ];
+              };
+              subPackages = ["cmd/exporter"];
+              vendorHash = "sha256-HiWXSvLwRzt1/wMl2LQfwrBPoRpsl5E3TsN/1N0PGWs=";
+              meta = exporter-cli.meta lib;
+            };
+            "${exporter-cli.name}" = pkgs.runCommand "${exporter-cli.name}" {} ''
+              mkdir -p $out/bin
+              cp ${exporter}/bin/exporter $out/bin/${exporter-cli.name}
+            '';
+          };
+          apps.exporter = {
+            meta = exporter-cli.meta lib;
+            type = "app";
+            program = "${self'.packages.${exporter-cli.name}}/bin/${exporter-cli.name}";
+          };
+          checks.exporter = pkgs.runCommand "exporter-help" {} ''
+            ${self'.packages.${exporter-cli.name}}/bin/${exporter-cli.name} --help > $out
+          # '';
+        };
+        flake = {};
+      };
 }
