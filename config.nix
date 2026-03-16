34326e65fcb6a1cbc0e3f9d20d5a10ddd126307f
SatabdiG <satabdi.ganguly@sap.com>
2026-03-16 14:19:58 +0100

    fix: update vendorHash for exporter-cli in config.nix

--- config.nix
+++ config.nix
@@ -3,7 +3,7 @@
   exporter-cli = {
     name = "xpcf";
     version = "0.0.1-alpha1";
-    vendorHash = "sha256-9JX69YJXrDAXoaBxITvkBTMLr/nlLYGEG1DV4Vswu8Q=";
+    vendorHash = "sha256-xmC9KCfHhP30fEuK9mEzM48Sc7YZPEyw7cW5o2VN71E=";
     # vendorHash = lib.fakeHash;
     meta = {
       description = "xpcf is a CLI tool for exporting existing resources as Crossplane managed resources";
