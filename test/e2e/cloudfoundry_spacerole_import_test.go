ca4cf047d49df816471b2348a4a13a19c105ef34
Nico Andres <nico.andres@protonmail.com>
2026-04-28 15:45:14 +0200

    Switch to dedicated users for orgrole + spacerole import

--- test/e2e/cloudfoundry_spacerole_import_test.go
+++ test/e2e/cloudfoundry_spacerole_import_test.go
@@ -13,7 +13,7 @@ import (
 var (
 	spaceRoleImportTestK8sResName = "e2e-test-space-role-import"
 	spaceRoleImportTestType       = "Developer"
-	spaceRoleImportTestUsername   = "user1@example.com"
+	spaceRoleImportTestUsername   = "e2e-test-space-role-import@example.com"
 	spaceRoleImportTestOrgName    = "cf-ci-e2e"
 	spaceRoleImportTestSpaceName  = "import-test-space-donotdelete"
 )
