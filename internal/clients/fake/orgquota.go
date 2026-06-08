4af0b28010f8a89ec984e3c748f9d2f9387c0fc1
Zoltán Szabó <12078386+Kukkerem@users.noreply.github.com>
2026-06-08 14:56:45 +0200

    OrgQuota External-Name Handling (#273)
    * feat(org): implement ADR-compliant external-name handling
    
    Signed-off-by: Zoltan Szabo <zoltan.szabo03@sap.com>
    
    * chore: generated CRD for OrgQuota external-name docs
    
    Signed-off-by: Zoltan Szabo <zoltan.szabo03@sap.com>
    
    * fix: reorder orgquota type description comments
    
    Signed-off-by: Zoltan Szabo <zoltan.szabo03@sap.com>
    
    ---------
    
    Signed-off-by: Zoltan Szabo <zoltan.szabo03@sap.com>

--- internal/clients/fake/orgquota.go
+++ internal/clients/fake/orgquota.go
@@ -3,6 +3,7 @@ package fake
 import (
 	"context"
 
+	"github.com/cloudfoundry/go-cfclient/v3/client"
 	"github.com/cloudfoundry/go-cfclient/v3/resource"
 	"github.com/stretchr/testify/mock"
 )
@@ -13,21 +14,29 @@ type MockOrgQuota struct {
 }
 
 func (m *MockOrgQuota) Get(ctx context.Context, guid string) (*resource.OrganizationQuota, error) {
-	args := m.Called(guid)
+	args := m.Called(ctx, guid)
 	return args.Get(0).(*resource.OrganizationQuota), args.Error(1)
 }
 
 func (m *MockOrgQuota) Create(ctx context.Context, opt *resource.OrganizationQuotaCreateOrUpdate) (*resource.OrganizationQuota, error) {
-	args := m.Called()
+	args := m.Called(ctx, opt)
 	return args.Get(0).(*resource.OrganizationQuota), args.Error(1)
 }
 
 func (m *MockOrgQuota) Update(ctx context.Context, guid string, opt *resource.OrganizationQuotaCreateOrUpdate) (*resource.OrganizationQuota, error) {
-	args := m.Called()
+	args := m.Called(ctx, guid, opt)
 	return args.Get(0).(*resource.OrganizationQuota), args.Error(1)
 }
 
 func (m *MockOrgQuota) Delete(ctx context.Context, guid string) (string, error) {
-	args := m.Called()
+	args := m.Called(ctx, guid)
 	return args.String(0), args.Error(1)
+}
+
+func (m *MockOrgQuota) Single(ctx context.Context, opts *client.OrganizationQuotaListOptions) (*resource.OrganizationQuota, error) {
+	args := m.Called(ctx, opts)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*resource.OrganizationQuota), args.Error(1)
 }
